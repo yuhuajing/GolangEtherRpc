b9e7449bf879dafb11d4ddb56348158dd3320a0c
yhj <yhj@yhjdeMacBook-Pro.local>
2023-05-17 19:20:00 +0800

    get storage slot

--- getSCStorage.go
+++ getSCStorage.go
@@ -8,33 +8,32 @@ import (
 	"github.com/ethereum/go-ethereum/common"
 )
 
-// var (
-// 	client  *ethclient.Client
-// 	account common.Address
-// )
+var (
+	address                 string
+	slot, highslot, lowslot int
+	arrayslot               string
+	//client                  *ethclient.Client
+	//account                 common.Address
+	ethServer string
+	blockNum  int64
+)
 
-// func init() {
-// 	client = getConn()
-// 	account = common.HexToAddress("0xC02aaA39b223FE8D0A0e5C4F27eAD9083C756Cc2")
-// }
-// func getConn() *ethclient.Client {
-// 	client, err := ethclient.Dial("https://cloudflare-eth.com")
-// 	if err != nil {
-// 		log.Fatal(err)
-// 	}
-// 	fmt.Println("we have a connection")
-// 	return client
-// }
-
-func getSCstorage() {
-
-	for i := int64(0); i < 4; i++ {
-		t := common.BigToHash(big.NewInt(i))
-		// fmt.Println(t)
-		res, _ := client.StorageAt(context.Background(), account, t, nil)
-		// StorageAt
-		fmt.Println(i)
-		fmt.Println(res)
+func getSCstorage(address common.Address, slot int, blockNum int64) {
+	t := common.BigToHash(big.NewInt(int64(slot)))
+	int256 := new(big.Int)
+	if blockNum != 0 {
+		//fmt.Printf("get slot %d of the address %s in the block %d\n", slot, address.Hex(), blockNum)
+		blocknumBigInt := big.NewInt(int64(blockNum))
+		res, _ := client.StorageAt(context.Background(), address, t, blocknumBigInt)
+		//	fmt.Println(res)
+		int256.SetBytes(res)
+	} else {
+		//fmt.Printf("get slot %d of the address %s in the latest block\n", slot, address.Hex())
+		res, _ := client.StorageAt(context.Background(), address, t, nil)
+		//	fmt.Println(res)
+		int256.SetBytes(res)
 	}
-
+	//fmt.Println()
+	fmt.Printf("0x%x\n", int256)
+	// fmt.Printf("uint256: %v\n", int256)
 }
