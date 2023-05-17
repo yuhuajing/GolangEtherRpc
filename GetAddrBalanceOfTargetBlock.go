ff9a45dcdb6652b7ec05660d97efbb92a3d50f71
ClayYu <32875832+yuhuajing@users.noreply.github.com>
2023-05-17 14:00:44 +0800

    Update GetAddrBalanceOfTargetBlock.go

--- GetAddrBalanceOfTargetBlock.go
+++ GetAddrBalanceOfTargetBlock.go
@@ -20,29 +20,7 @@ import (
 	"log"
 	"math"
 	"math/big"
-
-	"github.com/ethereum/go-ethereum/common"
-	"github.com/ethereum/go-ethereum/ethclient"
 )
-
-var (
-	client  *ethclient.Client
-	account common.Address
-)
-
-func init() {
-	client = getConn()
-	account = common.HexToAddress("0x71c7656ec7ab88b098defb751b7401b5f6d8976f")
-}
-
-func getConn() *ethclient.Client {
-	client, err := ethclient.Dial("https://cloudflare-eth.com")
-	if err != nil {
-		log.Fatal(err)
-	}
-	fmt.Println("we have a connection")
-	return client
-}
 
 func getAddrBalFronLatestBlock() {
 	balance, err := client.BalanceAt(context.Background(), account, nil)
