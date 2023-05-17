a4734ad67b2ac24047e9ad53de64d16a9cf56953
ClayYu <32875832+yuhuajing@users.noreply.github.com>
2023-05-17 14:02:17 +0800

    Update getTransactions.go

--- getTransactions.go
+++ getTransactions.go
@@ -3,31 +3,28 @@ package main
 import (
 	"context"
 	"fmt"
-	"log"
 	"math/big"
 
-	"github.com/ethereum/go-ethereum/common"
 	"github.com/ethereum/go-ethereum/core/types"
-	"github.com/ethereum/go-ethereum/ethclient"
 )
 
-var (
-	client  *ethclient.Client
-	account common.Address
-)
+// var (
+// 	client  *ethclient.Client
+// 	account common.Address
+// )
 
-func init() {
-	client = getConn()
-	account = common.HexToAddress("0x71c7656ec7ab88b098defb751b7401b5f6d8976f")
-}
-func getConn() *ethclient.Client {
-	client, err := ethclient.Dial("https://cloudflare-eth.com")
-	if err != nil {
-		log.Fatal(err)
-	}
-	fmt.Println("we have a connection")
-	return client
-}
+// func init() {
+// 	client = getConn()
+// 	account = common.HexToAddress("0x71c7656ec7ab88b098defb751b7401b5f6d8976f")
+// }
+// func getConn() *ethclient.Client {
+// 	client, err := ethclient.Dial("https://cloudflare-eth.com")
+// 	if err != nil {
+// 		log.Fatal(err)
+// 	}
+// 	fmt.Println("we have a connection")
+// 	return client
+// }
 
 func getTranctionsFromlatestBlock() {
 	block, _ := client.BlockByNumber(context.Background(), nil) //nil for the latest block
